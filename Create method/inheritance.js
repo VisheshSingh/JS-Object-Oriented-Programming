6a002fb75ed65ddae55292175fd0497cca53d78d
Vishesh <visheshsingh16@gmail.com>
2018-05-28 17:13:31 -0500

    Inheritance concept

---
+++ Create method/inheritance.js
@@ -0,0 +1,26 @@
+const Car = function(color) {
+  this.color = color;
+};
+
+Car.prototype = {
+  getColor() {
+    return this.color;
+  }
+};
+
+const ToyCar = function() {};
+
+ToyCar.prototype = Object.create(Car.prototype);
+
+let legoCar = new ToyCar();
+console.dir(legoCar);
+
+console.log(legoCar instanceof ToyCar);
+console.log(legoCar instanceof Car);
+console.log(legoCar instanceof Object);
+
+console.dir(ToyCar.prototype.isPrototypeOf(legoCar));
+
+console.dir(Car.prototype.isPrototypeOf(legoCar));
+
+console.dir(Object.prototype.isPrototypeOf(legoCar));
